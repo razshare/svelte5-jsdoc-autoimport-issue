4a2f669610502caa2bc148e53a8ef4e90092f68b
Razvan <razvan@razshare.dev>
2024-09-14 18:52:10 +0200

    first commit

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,16 @@
+<script>
+  /** @type {{header:Snippet,content:Snippet,footer:Snippet}}*/
+  let { header, content, footer } = $props();
+</script>
+
+<div class="card">
+  <div class="hreader">
+    {@render header()}
+  </div>
+  <div class="content">
+    {@render content()}
+  </div>
+  <div class="footer">
+    {@render footer()}
+  </div>
+</div>
